005be31da22c9738b95b32bc42e6367ea998aa4a
Ashish Chahal <ashish.chahal94@gmail.com>
2020-09-05 05:41:28 +0530

    render multiple posts using react hooks useState and rendering them dynamically using map

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,27 +2,27 @@ import React from 'react';
 import './Styles/Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({username, caption, imageUrl}) {
     return (
         <div className="post">
             <div className="post__header">
             <Avatar 
                 className="post__avatar"
-                alt="Ashish_hacker"
+                alt={username}
                 src="/static.jpg"
             />
-            <h3>Username</h3>
+            <h3>{username}</h3>
             </div>
             
             {/* {header -> avatar + username} */}
 
             <img 
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcToxjpNRTS8lgTN2AFMdpsBwvF5zguP2PJL-g&usqp=CAU" 
+                src={imageUrl}
                 alt="react-logo" 
                 className="post__image" />
             {/* image */}
 
-            <h4 className="post__text"><strong>Hacker</strong>: WOW..I am awake at 3 am. cannot sleep and coding..wonderful</h4>
+            <h4 className="post__text"><strong>{username}</strong>{caption}</h4>
             {/* username + caption */}
         </div>
     )
