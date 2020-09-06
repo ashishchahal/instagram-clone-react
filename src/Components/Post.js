1f79c01f0c46ba0d088b522aa6760840dea1b24b
Ashish Chahal <ashish.chahal94@gmail.com>
2020-09-06 22:50:09 +0530

    Modal added for sign up

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,27 +2,27 @@ import React from 'react';
 import './Styles/Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post({username, caption, imageUrl}) {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
             <div className="post__header">
-            <Avatar 
-                className="post__avatar"
-                alt={username}
-                src="/static.jpg"
-            />
-            <h3>{username}</h3>
+                <Avatar
+                    className="post__avatar"
+                    alt={username}
+                    src="/static.jpg"
+                />
+                <h3>{username}</h3>
             </div>
-            
+
             {/* {header -> avatar + username} */}
 
-            <img 
+            <img
                 src={imageUrl}
-                alt="react-logo" 
+                alt="react-logo"
                 className="post__image" />
             {/* image */}
 
-            <h4 className="post__text"><strong>{username}</strong>{caption}</h4>
+            <h4 className="post__text"><strong>{username} </strong> {caption}</h4>
             {/* username + caption */}
         </div>
     )
